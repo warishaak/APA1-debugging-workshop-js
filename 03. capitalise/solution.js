50406b7d5295535808e0fc50b5916eb769011d67
kaleemw <warisha.kaleem@booking.com>
2025-01-13 12:25:28 +0000

    Tests pass for 03.capitalise

--- 03. capitalise/solution.js
+++ 03. capitalise/solution.js
@@ -1,8 +1,23 @@
 function capitalise(word) {
   word[0] = word[0].toUpperCase();
-  return word;
+
+  return word.charAt(0).toUpperCase() + word.slice(1).toLowerCase();
+
 }
+
+console.log(capitalise("hello"));
+
+console.log(capitalise("PyThOn"));
 
 module.exports = { capitalise };
 
- 
+
+// word.charAt(0) - Gets the first character of the word at index 0
+// .toUpperCase() - Converts that first character to uppercase
+// word.slice(1) - slice of the word from index 1 to the end
+// .toLowerCase() - Converts the last part to lowercase
+
+
+
+
+
