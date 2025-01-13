975c3d6bf291310faee8a39ce75a4a0d15b954e5
kaleemw <warisha.kaleem@booking.com>
2025-01-13 12:01:33 +0000

    Tests pass for 02.increment

--- 02. increment/solution.js
+++ 02. increment/solution.js
@@ -1,10 +1,15 @@
-function increment(n) {
-  return n++;
+function increment(n)
+{
+  return ++n ;
 }
+
+console.log(increment(5));
 
 module.exports = { increment };
 
 // Remember to check the expected behaviour of this program first
 // Then call the function to check how it is actually working...
 
-// Write some comments down below with your findings... 
+// Write some comments down below with your findings...
+
+//++n or n + 1
